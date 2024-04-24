33f84050f7ffb8329ae402191fbcf3d44afc1859
flo1a <105285535+flo1a@users.noreply.github.com>
2024-04-24 09:05:52 +0200

    nj

--- main.php
+++ main.php
@@ -16,6 +16,12 @@ $conn = DriverManager::getConnection($connectionParams);
 $sql = "SELECT * FROM Game";
 $stmt = $conn->executeQuery($sql);
 
+
+$htmlTemplateGame = file_get_contents('game.html');
+
 while (($row = $stmt->fetchAssociative()) !== false) {
+    $html = $htmlTemplate;
+    $html = str_replace("{player1}", $row[''], $html);
+    $html = str_replace("{hotel_description}", $hotel['description'], $html);
     echo $row['Match_Date'];
 }
