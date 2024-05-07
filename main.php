34c14319378bd517f642d87d75738c2203893c45
flo1a <105285535+flo1a@users.noreply.github.com>
2024-05-07 12:55:54 +0200

    1

--- main.php
+++ main.php
@@ -26,10 +26,12 @@ $htmlTemplate = file_get_contents('matches.html');
 
 $htmlOut = '';
 $htmlFin = '';
+$i = 0;
 
 while (($row = $stmt->fetchAssociative()) !== false) {
     $html = $htmlTemplateGame;
-    $html = str_replace("{round}", $row['PK_Match_ID'], $html);
+    $i++;
+    $html = str_replace("{round}", $i, $html);
     $html = str_replace("{player1}", $row['P1_First_Name'] . ' "' . $row['P1_Nickname'] . '" ' . $row['P1_Last_Name'], $html);
     $html = str_replace("{symbol1}", $row['Symbol1'], $html);
     $html = str_replace("{player2}", $row['P2_First_Name'] . ' "' . $row['P2_Nickname'] . '" ' . $row['P2_Last_Name'], $html);
